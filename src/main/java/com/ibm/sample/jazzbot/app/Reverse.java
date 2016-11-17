0fdd18d9f10f5d67153f7f590d62526401ea781d
Cheok Swin Voon <cheoksv@my.ibm.com>
2016-11-17 15:51:13 +0800

    Rename of Reflect to Reverse

--- src/main/java/com/ibm/sample/jazzbot/app/Reverse.java
+++ src/main/java/com/ibm/sample/jazzbot/app/Reverse.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/reverse")
-public class Reflect extends HttpServlet {
+public class Reverse extends HttpServlet {
     private static final long serialVersionUID = 1L;
 	
 	 @Override
